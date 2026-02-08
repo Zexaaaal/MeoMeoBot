2539178190444bb81e63e235ecf19faefb995e28
Zexal <therealzexal@gmail.com>
2026-02-08 15:43:09 +0100

    cleanup cuz wtf

--- repro_ws.js
+++
@@ -1,9 +0,0 @@
-const WebSocket = require('ws');
-
-const ws = new WebSocket('wss://eventsub.wss.twitch.tv/ws');
-
-setTimeout(() => {
-    console.log('Closing socket while connecting...');
-    ws.removeAllListeners();
-    ws.close();
-}, 10);
